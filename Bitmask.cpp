6321fbe253f0c7d5100b32cc2da78a31203bf79b
Carson <31937503+Carson-Tang@users.noreply.github.com>
2019-02-15 16:31:23 -0500

    Create Bitmask.cpp

---
+++ Bitmask.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define INFINITE 0x3f3f3f3f
+#define MAXN 100001
+#define MOD 1000000007
+typedef long long ll;
+typedef pair<int,int>ii;
+
+int setBit(int n, int idx){ return n|(1<<idx);}
+int unsetBit(int n, int idx){ return n&~(1<<idx);}
+bool checkBit(int n, int idx){ return n&(1<<idx);}
+
+int main(){
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}
